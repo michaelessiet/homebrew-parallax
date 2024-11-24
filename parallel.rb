82ac737c99f3961c69397defe7b42217c1cc334b
michaelessiet <essiet2002@gmail.com>
2024-11-24 16:31:39 +0100

    fix: change name to parallax

--- parallel.rb
+++ parallel.rb
@@ -1,15 +1,11 @@
-class Parallel < Formula
-  desc "Parallel is a simple command line tool to run shell commands in parallel. Think GNU Parallel but simpler."
-  homepage "https://github.com/michaelessiet/parallel"
-  url "https://github.com/michaelessiet/parallel/archive/v0.1.0.tar.gz"
+class Parallax < Formula
+  desc "Parallax is a simple command line tool to run shell commands in parallel. Think GNU Parallel but simpler."
+  homepage "https://github.com/michaelessiet/homebrew-parallax"
+  url "https://github.com/michaelessiet/homebrew-parallax/archive/v0.1.0.tar.gz"
   sha256 "d3f056f2597297eee4e98ba00031003eab873d16f6a95c077956c7214f280634"
   license "MIT" # or whatever license you use
 
-  # depends_on "dependency1"
-  # depends_on "dependency2"
-
   depends_on "rust" => :build
-  # depends_on "cargo" => :build
 
   def install
     system "cargo", "install", "--root", prefix, "--path", "."
@@ -17,6 +13,6 @@ class Parallel < Formula
   end
 
   test do
-    system "#{bin}/parallel", "--version"
+    system "#{bin}/parallax", "--version"
   end
 end
